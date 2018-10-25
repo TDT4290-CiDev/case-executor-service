46719c25a7e4cd6b896df415e9e8357d9b400d2f
Eivind Lie Andreassen <ela4991@gmail.com>
2018-10-25 17:52:56 +0200

    Add executor (with outline of case execution flow)

---
+++ src/executor.py
@@ -0,0 +1,60 @@
+from case_collection import CaseCollection
+import requests
+
+case_collection = CaseCollection()
+
+block_url = 'workflow-block-service:8080/'
+
+
+def post_json(endpoint, body):
+    url = block_url + endpoint
+    response = requests.post(url, json=body)
+    return response.json()
+
+
+def add_case(workflow, input_data):
+    case = {
+        "workflow": workflow,
+        "data": input_data,
+        "step": 0,
+    }
+
+    case_collection.add_case(case)
+
+
+def execute_case(cid):
+    case = case_collection.get_case(cid)
+
+    # If another worker is executing the case, we will not
+    # TODO: Only fetch if not executing, and automatically set executing (to avoid concurrency problems)
+    if case['executing']:
+        return
+    case['executing'] = True
+    case_collection.update_case(cid, {'executing': True})
+
+    workflow = case['workflow']
+    step = case['step']
+
+    while True:
+        step_item = workflow['blocks'][step]
+
+        if step_item.type == "action":
+            params = step_item['params']
+            # TODO Insert data from data store and previous step
+            result = post_json(step_item['name'], {'params': params})
+
+            if result['type'] == 'result':
+                case['previous_outputs'] = result['data']
+
+                for saved_output, save_to in step_item['save_outputs'].items():
+                    if saved_output in result['data']:
+                        # TODO Split on '.' to generate deeper structure?
+                        case['store'][save_to] = result['data'][saved_output]
+                step = step_item['next_block']
+            elif result['type'] == 'suspend':
+                case['suspended'] = True
+                # TODO Need to save state, and handle the reason for suspension
+                pass
+
+        # Save the new state of the case (ensures that we can continue after a crash)
+        case_collection.update_case(cid, case)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+from http import HTTPStatus
+import executor
 
 app = Flask(__name__)
 
@@ -8,6 +10,14 @@ def hello_docker():
     return 'Hello Docker!'
 
 
+@app.route('/execute_workflow/<id>', methods=['POST'])
+def execute_workflow(id):
+    workflow = None  # TODO Fetch from workflow-editor-service
+    form_data = request.get_json()
+    executor.add_case(workflow, form_data)
+    return HTTPStatus.CREATED
+
+
 # Only for testing purposes - should use WSGI server in production
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5000)
