7a04138adbfd2af3ec9d4660973f30f2d997b5e0
Eivind Lie Andreassen <ela4991@gmail.com>
2018-10-30 15:39:36 +0100

    Add general try/except to avoid crashes on worker threads

--- src/executor.py
+++ src/executor.py
@@ -53,65 +53,68 @@ def execute_case(cid):
     if not case:
         return
 
-    workflow = case['workflow']
-    step = case['step']
+    try:
+        workflow = case['workflow']
+        step = case['step']
 
-    while True:
-        step_item = workflow['blocks'][step]
+        while True:
+            step_item = workflow['blocks'][step]
 
-        if step_item['type'] == 'action':
-            block_info = requests.get(block_url + step_item['name'] + '/info').json()
+            if step_item['type'] == 'action':
+                block_info = requests.get(block_url + step_item['name'] + '/info').json()
 
-            params = step_item['params']
-            insert_params = DotMap({'store': case['store'],
-                                    'outputs': case['previous_outputs']})
-            for p_name, p_value in params.items():
-                if type(p_value) == str:
-                    params[p_name] = p_value.format_map(insert_params)
+                params = step_item['params']
+                insert_params = DotMap({'store': case['store'],
+                                        'outputs': case['previous_outputs']})
+                for p_name, p_value in params.items():
+                    if type(p_value) == str:
+                        params[p_name] = p_value.format_map(insert_params)
 
-            cleaned_params = {}
-            for p in block_info['params']:
+                cleaned_params = {}
+                for p in block_info['params']:
+                    try:
+                        typ = type_map[block_info['params'][p]['type']]
+                        cleaned_params[p] = typ(params[p])
+                    except KeyError:
+                        case_error(case, 'Case is missing parameter ' + p + ' required by the block ' + step_item['name'] +
+                                   'in step ' + step)
+                        break
+                    except ValueError:
+                        case_error(case, 'Failed to cast parameter value for parameter ' + p + ' in step ' + step)
+                        break
+
                 try:
-                    typ = type_map[block_info['params'][p]['type']]
-                    cleaned_params[p] = typ(params[p])
-                except KeyError:
-                    case_error(case, 'Case is missing parameter ' + p + ' required by the block ' + step_item['name'] +
-                               'in step ' + step)
-                    break
-                except ValueError:
-                    case_error(case, 'Failed to cast parameter value for parameter ' + p + ' in step ' + step)
+                    result = post_json(step_item['name'], {'params': cleaned_params})
+                except Exception as e:
+                    case_error(case, str(e))
                     break
 
-            try:
-                result = post_json(step_item['name'], {'params': cleaned_params})
-            except Exception as e:
-                case_error(case, str(e))
+                if result['type'] == 'result':
+                    case['previous_outputs'] = result['data']
+
+                    store = DotMap(case['store'])
+                    for saved_output, save_to in step_item['save_outputs'].items():
+                        if saved_output in result['data']:
+                            path = save_to.split('.')
+                            item = store
+                            for i in range(len(path) - 1):
+                                item = item[path[i]]
+                            item[path[-1]] = result['data'][saved_output]
+                    step = step_item['next_block']
+                    case['store'] = store.toDict()
+                elif result['type'] == 'suspend':
+                    case['suspended'] = True
+                    # TODO Need to save state, and handle the reason for suspension
+                    pass
+
+            if step == '-1':
+                case['status'] = CaseStatus.FINISHED
+
+            # Save the new state of the case (ensures that we can continue after a crash)
+            case_collection.update_case(cid, case)
+
+            # If the next step is '-1', we have reached a terminal state
+            if step == '-1':
                 break
-
-            if result['type'] == 'result':
-                case['previous_outputs'] = result['data']
-
-                store = DotMap(case['store'])
-                for saved_output, save_to in step_item['save_outputs'].items():
-                    if saved_output in result['data']:
-                        path = save_to.split('.')
-                        item = store
-                        for i in range(len(path) - 1):
-                            item = item[path[i]]
-                        item[path[-1]] = result['data'][saved_output]
-                step = step_item['next_block']
-                case['store'] = store.toDict()
-            elif result['type'] == 'suspend':
-                case['suspended'] = True
-                # TODO Need to save state, and handle the reason for suspension
-                pass
-
-        if step == '-1':
-            case['status'] = CaseStatus.FINISHED
-
-        # Save the new state of the case (ensures that we can continue after a crash)
-        case_collection.update_case(cid, case)
-
-        # If the next step is '-1', we have reached a terminal state
-        if step == '-1':
-            break
+    except Exception as e:
+        case_error(case, 'An unexpected error occured: ' + str(e))
